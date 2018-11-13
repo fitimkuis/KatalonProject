378c62b93dd75c1b3e302dc53875d5ad1942d51e
fitimkuis <fitimkuis@gmail.com>
2018-11-13 17:17:10 +0200

    fix

--- Test Suites/Pdf.ts
+++ Test Suites/Pdf.ts
@@ -4,7 +4,7 @@
    <name>Pdf</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-11-12T19:32:01</lastRun>
+   <lastRun>2018-11-13T17:11:51</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
