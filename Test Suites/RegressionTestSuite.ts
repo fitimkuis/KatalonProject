32b06cfa000b7c71a915021da1536a41fd52e9e3
fitimkuis <fitimkuis@gmail.com>
2018-12-06 19:18:48 +0200

    fix

---
+++ Test Suites/RegressionTestSuite.ts
@@ -0,0 +1,103 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>RegressionTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>878609d6-c336-4e9c-a676-14383a3f65fb</testSuiteGuid>
+   <testCaseLink>
+      <guid>389f71ff-980b-4e88-9169-13ac64c4e166</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CucumberBDD/CucumberFeatureCall</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2dc06547-fe96-4499-902a-8d1eb8685bd7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/withoutWhiteSpaces</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>54e90cc7-e1e3-4eed-9dcc-1b7b6bbbf821</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/demotest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>20a1d9a7-07fb-4c1c-8791-4c73860b0f7e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ReadPdf/pdfReader</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4a78cf4f-ba67-40b8-9af1-83547c328ff5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Web Application/Data Driven Test case FOR LOOP</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3fce98b0-8ab8-451d-a18d-9f7ae6fd520f</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>ae1da9f3-dfc2-47bb-8485-2b1a858f44e4</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>62176495-edef-483c-9acd-1618dbbede97</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Web Application/datePicker</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4e275579-5e82-4663-b6da-baf6c6cc119f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Web Application/demoaut</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f403c489-bf2c-4f5d-bf0b-bf49397ea83b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Web Application/jQueryDatePicker</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>33eb3d4f-05ce-4ed3-b61a-0e4faddd1724</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>77f019d4-34b7-4a27-a205-bde89461f246</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Web Application/Kalaton 5.3 katalon demoaut</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4f48c4c1-a18c-49c6-b24c-e5c43844e927</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Web Application/kendo datepicker</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c55a668a-c300-4620-9dda-36cf25b14fae</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WebServiceTest/webService</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>44eb360c-8ccc-404b-b3c1-90d24db80943</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/XPATH Locator/xpath-locator</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
