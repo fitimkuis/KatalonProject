678c102e7f94a7c18a53f9e6b7b9978767234862
fitimkuis <fitimkuis@gmail.com>
2019-02-17 13:41:48 +0200

    fix

--- Scripts/Spam/spam/Script1549381527913.groovy
+++ Scripts/Spam/spam/Script1549381527913.groovy
@@ -1,10 +1,70 @@
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormatter
 import org.joda.time.format.ISODateTimeFormat
-import org.openqa.selenium.WebElement
+
+import com.kms.katalon.core.exception.StepErrorException
+import com.kms.katalon.core.util.KeywordUtil
+
+
+for (int count = 3; count >= 0; count--) {
+	KeywordUtil.logInfo("count: $count")
+}
+KeywordUtil.logInfo("done")
+
+//WebUI.openBrowser('')
 
 //import com.gargoylesoftware.htmlunit.javascript.host.intl.DateTimeFormat
 //import com.github.jaiimageio.plugins.tiff.BaselineTIFFTagSet.DateTime
+
+
+/*
+WebUI.openBrowser('https://www.katalon.com/')
+def driver = DriverFactory.getWebDriver()
+String baseUrl = "https://www.katalon.com/"
+selenium = new WebDriverBackedSelenium(driver, baseUrl)
+selenium.openWindow("openWindow", "google")
+selenium.selectWindow("google")
+selenium.open("https://www.google.com/")
+selenium.runScript("window.test = 5")
+selenium.runScript("return window.test ${testvar}")
+System.out.println("${testvar}");
+// selenium.()
+*/
+//runScript javascript{alert(" 'DO YOU LIKE ALL THESE EXAMPLES? :) IF YES THEN SUBSCRIBE VIA EMAIL TO GET MORE SUCH EXAMPLES ON YOUR EMAIL ID' ")}
+String expectedString = "two"
+
+List<String> myArray = new ArrayList<>()
+myArray.add("one")
+myArray.add("two")
+myArray.add("three")
+
+if (myArray.contains(expectedString)){
+	println "Value is in array"
+}
+else{
+	println "Value is not in array"
+}
+
+boolean val = false
+
+//try {
+	for (String item: myArray){
+		if (item.equals(expectedString)){
+			println "Value is in array: "+item
+			val = true
+			break;
+		}
+	}
+		if (!val){
+			throw new com.kms.katalon.core.exception.StepErrorException('test failed expected string is not in array')
+		}
+	/*} catch (com.kms.katalon.core.exception.StepErrorException e){
+		this.println(e)
+	} catch (Exception e){
+		this.println ("Test Failed general issue")
+	} finally {
+		println "expected string is in array "
+	}*/
 
 
 def firstEventDateTime= "2019-02-04T12:01:08.397106+00:00";
--- Scripts/WebServiceTest/webService/Script1522654322411.groovy
+++ Scripts/WebServiceTest/webService/Script1522654322411.groovy
@@ -1,8 +1,46 @@
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 
+import com.kms.katalon.core.logging.KeywordLogger
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+
 def response = WS.sendRequest(findTestObject('WebServiceReqs/webRequest'))
+def responseText = response.getResponseText()
+//println responseText
+
+println JsonOutput.prettyPrint(responseText)
+
+def jsonSlurper = new JsonSlurper()
+def object = jsonSlurper.parseText(responseText)
+int jsonSize = object.MRData.CircuitTable.Circuits.circuitId.size()
+
+KeywordLogger log = new KeywordLogger()
+log.logInfo("count of json side "+jsonSize)
+
+//println "count of json side "+jsonSize
+
+for (int i = 0; i < jsonSize+1; i++){
+	println(object.MRData.CircuitTable.Circuits.circuitId[i])
+}
+//def text = {"applications":[{"name":"test123","id":"c1257c5","description":"test","type":"generic","version":"0.1"},{"name":"Asset_1","id":"a9e0bce","description":"sfsdgdg","type":"generic","version":"0.1"},{"name":"Asset_2","id":"a9e0cd2","description":"sffgdgf","type":"generic","version":"0.1"}]}
+
+def json = new JsonSlurper().parseText('{"applications":[{"name":"test123","id":"c1257c5","description":"test","type":"generic","version":"0.1"},{"name":"Asset_1","id":"a9e0bce","description":"sfsdgdg","type":"generic","version":"0.1"},{"name":"Asset_2","id":"a9e0cd2","description":"sffgdgf","type":"generic","version":"0.1"}]}')
+println(json.applications[1].id)
+/*println JsonOutput.prettyPrint(text)
+def jsonSlurper = new JsonSlurper()
+def object = jsonSlurper.parseText(text)*/
+
+//assert object.response[0].id == 'c1257c5'
+//println object.get("applications").get(0).get("id")
+//JsonSlurper slurper = new JsonSlurper()
+//Map parsedJson = slurper.parseText(response)
+//println parsedJson.get("applications").get(0).get("id")
+
+println "response: $response"
+def cookiez = response.getHeaderFields()['Set-Cookie']
+println cookiez
 WS.verifyResponseStatusCode(response, 200)
 WS.containsString(response, 'Bahrain International Circuit', false)
 
