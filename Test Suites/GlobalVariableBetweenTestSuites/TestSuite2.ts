4221c10a125b97017a83f5f3ec895276c1a69641
fitimkuis <fitimkuis@gmail.com>
2019-04-22 11:31:11 +0300

    new testcases

---
+++ Test Suites/GlobalVariableBetweenTestSuites/TestSuite2.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>65b130c6-80ef-40b4-bc24-d2b53b97d602</testSuiteGuid>
+   <testCaseLink>
+      <guid>e2b3e21e-43af-44ca-8c36-33478843c60a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GlobalVarBetweenTestSuites/forTestSuite2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
