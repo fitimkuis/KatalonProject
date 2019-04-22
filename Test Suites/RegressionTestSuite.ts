9dca0d168b2aca26a4b8be27bf5e435d8d4aa118
fitimkuis <fitimkuis@gmail.com>
2019-04-22 11:30:24 +0300

    fix 5

--- Test Suites/RegressionTestSuite.ts
+++ Test Suites/RegressionTestSuite.ts
@@ -13,13 +13,13 @@
    <testCaseLink>
       <guid>389f71ff-980b-4e88-9169-13ac64c4e166</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/CucumberBDD/CucumberFeatureCall</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>2dc06547-fe96-4499-902a-8d1eb8685bd7</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Demo/withoutWhiteSpaces</testCaseId>
    </testCaseLink>
    <testCaseLink>
@@ -31,13 +31,13 @@
    <testCaseLink>
       <guid>20a1d9a7-07fb-4c1c-8791-4c73860b0f7e</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/ReadPdf/pdfReader</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>4a78cf4f-ba67-40b8-9af1-83547c328ff5</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Web Application/Data Driven Test case FOR LOOP</testCaseId>
       <variableLink>
          <testDataLinkId></testDataLinkId>
@@ -55,19 +55,19 @@
    <testCaseLink>
       <guid>62176495-edef-483c-9acd-1618dbbede97</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Web Application/datePicker</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>4e275579-5e82-4663-b6da-baf6c6cc119f</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Web Application/demoaut</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>f403c489-bf2c-4f5d-bf0b-bf49397ea83b</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Web Application/jQueryDatePicker</testCaseId>
       <variableLink>
          <testDataLinkId></testDataLinkId>
@@ -85,19 +85,19 @@
    <testCaseLink>
       <guid>4f48c4c1-a18c-49c6-b24c-e5c43844e927</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Web Application/kendo datepicker</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>c55a668a-c300-4620-9dda-36cf25b14fae</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/WebServiceTest/webService</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>44eb360c-8ccc-404b-b3c1-90d24db80943</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/XPATH Locator/xpath-locator</testCaseId>
    </testCaseLink>
    <testCaseLink>
