8db2554a88b5e1bebb2c3c251a8491e43f5ea872
fitimkuis <fitimkuis@gmail.com>
2019-05-26 10:30:54 +0300

    only one testcase in testsuite

--- Test Suites/RegressionTestSuite.ts
+++ Test Suites/RegressionTestSuite.ts
@@ -13,13 +13,13 @@
    <testCaseLink>
       <guid>389f71ff-980b-4e88-9169-13ac64c4e166</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/CucumberBDD/CucumberFeatureCall</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>4a78cf4f-ba67-40b8-9af1-83547c328ff5</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Web Application/Data Driven Test case FOR LOOP</testCaseId>
       <variableLink>
          <testDataLinkId></testDataLinkId>
@@ -37,25 +37,25 @@
    <testCaseLink>
       <guid>54e90cc7-e1e3-4eed-9dcc-1b7b6bbbf821</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Demo/demotest</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>20a1d9a7-07fb-4c1c-8791-4c73860b0f7e</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/ReadPdf/pdfReader</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>2dc06547-fe96-4499-902a-8d1eb8685bd7</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Demo/withoutWhiteSpaces</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>62176495-edef-483c-9acd-1618dbbede97</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Web Application/datePicker</testCaseId>
    </testCaseLink>
    <testCaseLink>
@@ -67,7 +67,7 @@
    <testCaseLink>
       <guid>f403c489-bf2c-4f5d-bf0b-bf49397ea83b</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Web Application/jQueryDatePicker</testCaseId>
       <variableLink>
          <testDataLinkId></testDataLinkId>
@@ -85,25 +85,25 @@
    <testCaseLink>
       <guid>4f48c4c1-a18c-49c6-b24c-e5c43844e927</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Web Application/kendo datepicker</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>c55a668a-c300-4620-9dda-36cf25b14fae</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/WebServiceTest/webService</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>44eb360c-8ccc-404b-b3c1-90d24db80943</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/XPATH Locator/xpath-locator</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>aae2f9bd-05fd-4804-85d0-c2203192fccf</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/WriteToExcel(SpreadSheet)/write-to-excel-spreadsheet</testCaseId>
    </testCaseLink>
    <testCaseLink>
@@ -115,7 +115,7 @@
    <testCaseLink>
       <guid>5ff7ff57-2fb6-485a-bbcb-56cf6c3b4c80</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/LogInKatalon/Call Test Case Login</testCaseId>
       <variableLink>
          <testDataLinkId></testDataLinkId>
@@ -133,31 +133,31 @@
    <testCaseLink>
       <guid>93b19663-d59d-4111-a89a-b5821786aa48</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/LogInKatalon/Login</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>cf9653cc-ca99-43f0-a179-fc022f152851</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/SpamData/spamDataCheck</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>f65f6328-9aef-4f07-926c-a909d5c5498d</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/CygwinSSH/cygwinSsh</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>77401652-72e3-48b4-9247-8765c1e416a4</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/CaptureAlert/alertCapture</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>301bb606-d36c-4837-98a5-9e26a6518ff9</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/GetAttributeValue/getAttribute</testCaseId>
    </testCaseLink>
    <testCaseLink>
@@ -169,7 +169,7 @@
    <testCaseLink>
       <guid>dab5cac0-4014-45ba-a455-b203a24ef5d0</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/IniFileUtil/iniFile</testCaseId>
    </testCaseLink>
    <testCaseLink>
@@ -181,31 +181,31 @@
    <testCaseLink>
       <guid>943578ac-fd98-4cc4-b3f1-b7f811d5f9bc</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/PdfUtil/mergePdfFiles</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>44872692-bbec-4637-bbd4-09a7973688a7</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/GnerateSIN/generateSin</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>db9219cd-ca7b-4bf0-a354-fe369cacec6c</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/ReadYAML/readyaml</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>8e967a02-264d-48c7-80e3-727b8a27ef2f</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/PropertiesFile/properties</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>685a9762-bc9e-478b-ba26-c51b7d3a6e4a</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/IsFileDownloaded/fileDownload</testCaseId>
    </testCaseLink>
    <testCaseLink>
@@ -235,7 +235,7 @@
    <testCaseLink>
       <guid>a9433d4d-2584-40d1-b2c2-fa682545513b</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/TestCaseParameters/dataBinding</testCaseId>
       <testDataLink>
          <combinationType>ONE</combinationType>
@@ -256,13 +256,13 @@
    <testCaseLink>
       <guid>54d09304-5de4-4ebb-a952-0fbce8b5a339</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/ChromeDriverTest/CreateChromeDriver</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>c095917c-f814-4b7c-9a1e-2bce659346ac</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/ExcelDataBinding/databindFromExcel</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
