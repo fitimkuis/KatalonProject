35550b9bc812a5faeb16e9031fca72bc733ea448
fitim <fitimkuis@gmail.com>
2020-02-09 14:19:21 +0200

    datetime something more

--- Scripts/ChromeCustomProfiler/example_openChromeDriverWithDefaultProfile/Script1581110790656.groovy
+++ Scripts/ChromeCustomProfiler/example_openChromeDriverWithDefaultProfile/Script1581110790656.groovy
@@ -1,27 +1,8 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
-
 import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
 
-//import com.kazurayam.ks.thoughtful.ChromeDriverFactory
-import com.kazurayam.webdriverfactory4ks.ChromeDriverFactory
-//import com.kazurayam.ks.thoughtful.ChromeProfileFinder
 import com.kazurayam.webdriverfactory4ks.ChromeProfileFinder
-import com.kms.katalon.core.webui.driver.DriverFactory
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 /**
@@ -31,6 +12,18 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
  * so usually be there.
  * If the directory 'User Data\Default' is not found, an Exception will be thrown.
  */
+
+ChromeOptions options = new ChromeOptions();
+options.addArguments("--user-data-dir=C:/Users/fitim/AppData/Local/Google/Chrome/User Data/Default");
+options.addArguments("--disable-extensions");
+WebDriver driver = new ChromeDriver(options)
+driver.get("http://demoaut.katalon.com/");
+String profileName = ChromeProfileFinder.getChromeProfileNameByDirectoryName('Default')
+WebUI.comment("Directory \'Default\' is associated with Chrome Profile \'${profileName}\'")
+WebUI.delay(3)
+//.quit()
+
+/*
 ChromeDriverFactory cdFactory = new ChromeDriverFactory()
 WebDriver driver = cdFactory.openChromeDriverWithProfileDirectory('Default')
 assert driver != null
@@ -41,4 +34,4 @@ String profileName = ChromeProfileFinder.getChromeProfileNameByDirectoryName('De
 WebUI.comment("Directory \'Default\' is associated with Chrome Profile \'${profileName}\'")
 
 WebUI.delay(3)
-WebUI.closeBrowser()
+WebUI.closeBrowser()*/
