fe8f0ec3743a5eaa271fdde35a307a1b010b3e67
fitim <fitimkuis@gmail.com>
2020-12-03 21:27:54 +0200

    firefox popup ignore

---
+++ Scripts/FirefoxBrowser/FirefoxNoPopUp/Script1607015327219.groovy
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.firefox.FirefoxProfile
+import org.openqa.selenium.remote.DesiredCapabilities
+
+
+WebDriver driver ;
+FirefoxProfile profile = new FirefoxProfile();
+profile.setPreference("permissions.default.desktop-notification", 1);
+DesiredCapabilities capabilities=DesiredCapabilities.firefox();
+capabilities.setCapability(FirefoxDriver.PROFILE, profile);
+driver = new FirefoxDriver(capabilities);
+//driver.get("http://google.com");
+driver.get("http://demoaut.katalon.com/");
+driver.quit()
+
+FirefoxProfile geoDisabled = new FirefoxProfile();
+geoDisabled.setPreference("geo.enabled", false);
+geoDisabled.setPreference("geo.provider.use_corelocation", false);
+geoDisabled.setPreference("geo.prompt.testing", false);
+geoDisabled.setPreference("geo.prompt.testing.allow", false);
+capabilities = DesiredCapabilities.firefox();
+capabilities.setCapability(FirefoxDriver.PROFILE, geoDisabled);
+driver = new FirefoxDriver(capabilities);
+driver.get("http://demoaut.katalon.com/");
+driver.quit()
